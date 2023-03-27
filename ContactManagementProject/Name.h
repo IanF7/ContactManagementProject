4380d36d26d1fe9ab49f24f6996997070f5f4e0b
Ian Fletcher <IanF0347@gmail.com>
2023-03-27 12:45:45 -0700

    Add project files.

---
+++ ContactManagementProject/Name.h
@@ -0,0 +1,31 @@
+//Ian Fletcher
+//This is mostly my own code with help from Professor Fritz
+#ifndef NAME_H
+#define NAME_H
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+class Name
+{
+private:
+	//creates data members for firstName and lastName
+	string firstName, lastName;
+public:
+	//default constructor
+	Name();
+	//parameterized constructor
+	Name(string firstName, string lastName);
+	//getters and setters
+	string getFirstName() const;
+	void setFirstName(string firstName);
+	string getLastName() const;
+	void setLastName(string lastName);
+	string getFullName() const;
+	//friend overloaded functions to control outputs and inputs
+	friend ostream& operator<< (ostream& out, const Name& rhs);
+	friend istream& operator >> (istream& in, Name& rhs);
+};
+
+#endif
